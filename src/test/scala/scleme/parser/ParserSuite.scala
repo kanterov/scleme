03261d751edb7fc0715b7996f38298eeb80d6b99
Gleb Kanterov <gleb@kanterov.ru>
2012-11-05 20:00:49 +0700

    + parser tests

--- src/test/scala/scleme/parser/ParserSuite.scala
+++ src/test/scala/scleme/parser/ParserSuite.scala
@@ -9,7 +9,11 @@ class ParserSuite extends FunSuite with ShouldMatchers {
   val inputOutput = Map(
     "0" -> NumExpr(0),
     "-1" -> NumExpr(-1),
-    "1" -> NumExpr(1))
+    "1" -> NumExpr(1),
+    "#t" -> BoolExpr(true),
+    "#f" -> BoolExpr(false),
+    "'(1 2 3)" -> ListExpr(List(SymbolExpr("quote"), ListExpr(List(NumExpr(1), NumExpr(2), NumExpr(3))))),
+    "(1 2 3)" -> ListExpr(List(NumExpr(1), NumExpr(2), NumExpr(3))))
 
   def parse(input: String): Expr = Reader.apply(input).toOption.get
 
