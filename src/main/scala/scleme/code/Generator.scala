cc3210162348966133c06ed3c95aff49c760d4b8
Gleb Kanterov <gleb@kanterov.ru>
2012-11-05 21:14:18 +0700

    + support for and/or/not on booleans

--- src/main/scala/scleme/code/Generator.scala
+++ src/main/scala/scleme/code/Generator.scala
@@ -12,33 +12,43 @@ object Generator {
       (DEF("apply") := apply0(expr)),
       (DEF("main") withParams (PARAM("args", TYPE_ARRAY(StringClass))) := BLOCK()))
   }
-  
+
   def isOp(x: String): Boolean = List("+", "-", "/", "%", "*").contains(x)
 
   def apply0(expr: List[Expr]): Seq[Tree] = expr.map(apply0)
-  
+
   def apply0(expr: Expr*): Seq[Tree] = expr.map(apply0)
-  
+
   def apply0(expr: Expr): Tree = {
     expr match {
       case NumExpr(i) => LIT(i)
-      
+
       case BoolExpr(b) => LIT(b)
-      
+
       case ListExpr(SymbolExpr(op) :: rest) if isOp(op) =>
-          INFIX_CHAIN(op, apply0(rest))
-          
+        INFIX_CHAIN(op, apply0(rest))
+
       case ListExpr(SymbolExpr("=") :: a :: b :: Nil) =>
-          INFIX_CHAIN("==", apply0(a, b))
+        INFIX_CHAIN("==", apply0(a, b))
 
       case ListExpr(SymbolExpr("char->fixnum") :: a :: Nil) =>
-          (REF("Core") DOT "charToFixnum") APPLY (apply0(a))
-          
+        (REF("Core") DOT "charToFixnum") APPLY (apply0(a))
+
       case ListExpr(SymbolExpr("fixnum->char") :: a :: Nil) =>
-          (REF("Core") DOT "fixnumToChar") APPLY (apply0(a))
-      
+        (REF("Core") DOT "fixnumToChar") APPLY (apply0(a))
+
       case ListExpr(SymbolExpr("fixnum?") :: a :: Nil) =>
-          (REF("Core") DOT "isFixnum") APPLY (apply0(a))
+        (REF("Core") DOT "isFixnum") APPLY (apply0(a))
+
+      case ListExpr(SymbolExpr("and") :: rest) =>
+        INFIX_CHAIN("&&", apply0(rest))
+
+      case ListExpr(SymbolExpr("or") :: rest) =>
+        INFIX_CHAIN("||", apply0(rest))
+
+      case ListExpr(SymbolExpr("not") :: a :: Nil) =>
+        (NOT(apply0(a)))
+
     }
   }
 
--- src/test/scala/scleme/BooleanOpSuite.scala
+++ src/test/scala/scleme/BooleanOpSuite.scala
@@ -6,4 +6,20 @@ import org.scalatest.matchers.ShouldMatchers
 
 class BooleanOpSuite extends FunSuite with ShouldMatchers with SclemeEval {
 
+  val inputOutput = Map(
+    "(and #t #t)" -> true,
+    "(and #t #f)" -> false,
+    "(and #f #f)" -> false,
+    "(and #t #t #f)" -> false,
+    "(or #t #t)" -> true,
+    "(or #t #f)" -> true,
+    "(or #f #f)" -> false,
+    "(or #f #f #t)" -> true)
+
+  for ((input, output) <- inputOutput) {
+    test("booleanOp " + input + " -> " + output) {
+      eval[Boolean](input) should equal(output)
+    }
+  }
+
 }
